03b322ede6371ada98e8c2824a2e562bc4a101e6
leandrodeveng <leandro.lt187@gmail.com>
2019-11-03 19:40:38 -0300

    exceptionsTreatment-EnvironmentVars

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '1f3b8c6defe5d9',
-    pass: 'a463002575bfee',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
